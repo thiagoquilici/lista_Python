c9c7a6e014c30a28f6061f2fa7428879de3d5579
thiagoquilici <thiagoquilici@gmail.com>
2024-05-24 20:26:13 -0300

    Versão 1.1.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+# importar biblioteca
+import os
+
 # lista vazia
 lista_de_tarefas = []
 
 # laço de repetição
 while True:
 # o usuário insere a nova tarefa na lista    
-    nova_tarefa = input('Informe a nova tarefa ou deixe vazio para encerrar e exibir a lista: ')
+    nova_tarefa = input('Insira a nova tarefa ou Enter para encerrar e exibir a lista: ')
 
     # verifica se o usuário inseriu nova tarefa
     if nova_tarefa != '':
@@ -14,8 +17,13 @@ while True:
         continue
     else:
         break
-   
+
+# limpa console
+os.system('cls')
+
 # exibe na tela a lista
+print(f'{'-'*30} LISTA DE TAREFAS {'-'*30}')   
+
 for tarefa in lista_de_tarefas:
         print(tarefa)
    
